489e1fa8964077802ed2bb643a8a64ed871ecbde
Camilo Ramirez <91098453+ramirezdiazj5@users.noreply.github.com>
2022-10-19 16:00:26 -0500

    Update AnalisisSentimientoCovid19.py

--- AnalisisWeb/AnalisisSentimientoCovid19.py
+++ AnalisisWeb/AnalisisSentimientoCovid19.py
@@ -13,7 +13,7 @@ with open('modelo_randomForest.pickle', 'rb') as file:
     modelo = pickle.load(file)
 
 # Carga de dataset
-datasetModelo = pd.read_csv(r'AnalisisWeb/Tweets_Prediccion.csv')
+datasetModelo = pd.read_csv('Tweets_Prediccion.csv')
 
 # Funcion de clasificacion de sentimientos
 def clasificacion(sentimiento):
